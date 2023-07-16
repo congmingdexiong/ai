5fd5f031cd203b2f7b4d4a78eaaf86ea0c25689f
aleng1106 <ltlobygg@163.com>
2023-07-16 10:44:01 +0800

    change

---
+++ lang-chain-for-llm-application-development/lesson-4-chains/main.py
@@ -0,0 +1,283 @@
+import openai
+import os
+
+from dotenv import load_dotenv, find_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+# 导入 OpenAI API_KEY
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+import pandas as pd
+df = pd.read_csv('Data.csv')
+
+print(df)
+
+from langchain.chat_models import ChatOpenAI    #导入OpenAI模型
+from langchain.prompts import ChatPromptTemplate   #导入聊天提示模板
+from langchain.chains import LLMChain    #导入LLM链。
+
+llm = ChatOpenAI(temperature=0.9)
+prompt = ChatPromptTemplate.from_template('what is the best name to describe a company that makes {product}?')
+
+chain = LLMChain(llm=llm,prompt=prompt)
+product = 'Queen size sheet set'
+
+print(chain.run(product))
+
+# sequential chains
+
+from langchain.chains import SimpleSequentialChain
+llm = ChatOpenAI(temperature = 0.9)
+
+
+first_prompt = ChatPromptTemplate.from_template("Translate the following review to english:" "\n\n{Review}")
+#chain 1
+chain_one = LLMChain(llm=llm, prompt=first_prompt)
+
+
+
+
+second_prompt = ChatPromptTemplate.from_template(
+    "Write a 20 words description for the following \
+    company:{company_name}"
+)
+# chain 2
+chain_two = LLMChain(llm=llm, prompt=second_prompt)
+overall_simple_chain = SimpleSequentialChain(chains=[chain_one,chain_two],verbose=True)
+print(overall_simple_chain.run(product))
+
+
+### 2.2 SequentialChain
+
+from langchain.chains import SequentialChain
+from langchain.chat_models import ChatOpenAI    #导入OpenAI模型
+from langchain.prompts import ChatPromptTemplate   #导入聊天提示模板
+from langchain.chains import LLMChain    #导入LLM链。
+
+#子链1
+
+# prompt模板 1: 翻译成英语（把下面的review翻译成英语）
+first_prompt = ChatPromptTemplate.from_template(
+    "Translate the following review to english:"
+    "\n\n{Review}"
+)
+# chain 1: 输入：Review 输出： 英文的 Review
+chain_one = LLMChain(llm=llm, prompt=first_prompt,
+                     output_key="English_Review"
+                    )
+
+#子链2
+
+# prompt模板 2: 用一句话总结下面的 review
+second_prompt = ChatPromptTemplate.from_template(
+    "Can you summarize the following review in 1 sentence:"
+    "\n\n{English_Review}"
+)
+# chain 2: 输入：英文的Review   输出：总结
+chain_two = LLMChain(llm=llm, prompt=second_prompt,
+                     output_key="summary"
+                    )
+#子链3
+
+# prompt模板 3: 下面review使用的什么语言
+third_prompt = ChatPromptTemplate.from_template(
+    "What language is the following review:\n\n{Review}"
+)
+# chain 3: 输入：Review  输出：语言
+chain_three = LLMChain(llm=llm, prompt=third_prompt,
+                       output_key="language"
+                      )
+
+#子链4
+
+# prompt模板 4: 使用特定的语言对下面的总结写一个后续回复
+fourth_prompt = ChatPromptTemplate.from_template(
+    "Write a follow up response to the following "
+    "summary in the specified language:"
+    "\n\nSummary: {summary}\n\nLanguage: {language}"
+)
+# chain 4: 输入： 总结, 语言    输出： 后续回复
+chain_four = LLMChain(llm=llm, prompt=fourth_prompt,
+                      output_key="followup_message"
+                     )
+
+
+# 对四个子链进行组合
+
+#输入：review    输出：英文review，总结，后续回复
+overall_chain = SequentialChain(
+    chains=[chain_one, chain_two, chain_three, chain_four],
+    input_variables=["Review"],
+    output_variables=["English_Review", "summary","followup_message"],
+    verbose=True)
+
+
+review = df.Review[5]
+overall_chain(review)
+
+print("SequentialChain:")
+print(review)
+print(overall_chain(review))
+
+
+
+# Router chain
+
+physics_template = """You are a very smart physics professor. \
+You are great at answering questions about physics in a concise\
+and easy to understand manner. \
+When you don't know the answer to a question you admit\
+that you don't know.
+
+Here is a question:
+{input}"""
+
+
+#第二个提示适合回答数学问题
+math_template = """You are a very good mathematician. \
+You are great at answering math questions. \
+You are so good because you are able to break down \
+hard problems into their component parts, 
+answer the component parts, and then put them together\
+to answer the broader question.
+
+Here is a question:
+{input}"""
+
+
+#第三个适合回答历史问题
+history_template = """You are a very good historian. \
+You have an excellent knowledge of and understanding of people,\
+events and contexts from a range of historical periods. \
+You have the ability to think, reflect, debate, discuss and \
+evaluate the past. You have a respect for historical evidence\
+and the ability to make use of it to support your explanations \
+and judgements.
+
+Here is a question:
+{input}"""
+
+
+#第四个适合回答计算机问题
+computerscience_template = """ You are a successful computer scientist.\
+You have a passion for creativity, collaboration,\
+forward-thinking, confidence, strong problem-solving capabilities,\
+understanding of theories and algorithms, and excellent communication \
+skills. You are great at answering coding questions. \
+You are so good because you know how to solve a problem by \
+describing the solution in imperative steps \
+that a machine can easily interpret and you know how to \
+choose a solution that has a good balance between \
+time complexity and space complexity. 
+
+Here is a question:
+{input}"""
+
+prompt_infos = [
+    {
+        "name": "physics",
+        "description": "Good for answering questions about physics",
+        "prompt_template": physics_template
+    },
+    {
+        "name": "math",
+        "description": "Good for answering math questions",
+        "prompt_template": math_template
+    },
+    {
+        "name": "History",
+        "description": "Good for answering history questions",
+        "prompt_template": history_template
+    },
+    {
+        "name": "computer science",
+        "description": "Good for answering computer science questions",
+        "prompt_template": computerscience_template
+    }
+
+]
+from langchain.chains.router import MultiPromptChain  #导入多提示链
+from langchain.chains.router.llm_router import LLMRouterChain,RouterOutputParser
+from langchain.prompts import PromptTemplate
+
+llm = ChatOpenAI(temperature=0)
+
+destination_chains = {}
+for p_info in prompt_infos:
+    name = p_info["name"]
+    prompt_template = p_info["prompt_template"]
+    prompt = ChatPromptTemplate.from_template(template=prompt_template)
+    chain = LLMChain(llm=llm, prompt=prompt)
+    destination_chains[name] = chain
+
+destinations = [f"{p['name']}: {p['description']}" for p in prompt_infos]
+destinations_str = "\n".join(destinations)
+
+default_prompt = ChatPromptTemplate.from_template("{input}")
+default_chain = LLMChain(llm=llm, prompt=default_prompt)
+
+MULTI_PROMPT_ROUTER_TEMPLATE = """Given a raw text input to a \
+language model select the model prompt best suited for the input. \
+You will be given the names of the available prompts and a \
+description of what the prompt is best suited for. \
+You may also revise the original input if you think that revising\
+it will ultimately lead to a better response from the language model.
+
+<< FORMATTING >>
+Return a markdown code snippet with a JSON object formatted to look like:
+```json
+{{{{
+    "destination": string \ name of the prompt to use or "DEFAULT"
+    "next_inputs": string \ a potentially modified version of the original input
+}}}}
+```
+
+REMEMBER: "destination" MUST be one of the candidate prompt \
+names specified below OR it can be "DEFAULT" if the input is not\
+well suited for any of the candidate prompts.
+REMEMBER: "next_inputs" can just be the original input \
+if you don't think any modifications are needed.
+
+<< CANDIDATE PROMPTS >>
+{destinations}
+
+<< INPUT >>
+{{input}}
+
+<< OUTPUT (remember to include the ```json)>>"""
+
+router_template = MULTI_PROMPT_ROUTER_TEMPLATE.format(
+    destinations=destinations_str
+)
+router_prompt = PromptTemplate(
+    template=router_template,
+    input_variables=["input"],
+    output_parser=RouterOutputParser(),
+)
+
+router_chain = LLMRouterChain.from_llm(llm, router_prompt)
+
+#多提示链
+chain = MultiPromptChain(router_chain=router_chain,    #l路由链路
+                         destination_chains=destination_chains,   #目标链路
+                         default_chain=default_chain,      #默认链路
+                         verbose=True
+                        )
+
+# 问题：什么是黑体辐射？
+print(chain.run("What is black body radiation?"))
+
+#中文
+print(chain.run("黑洞是种什么物质?"))
+
+# 问题：2+2等于多少？
+print(chain.run("what is 2 + 2"))
+
+# 问题：为什么我们身体里的每个细胞都包含DNA？
+print(chain.run("Why does every cell in our body contain DNA?"))
+
+# 中文
+print(chain.run("你知道李白是谁嘛?"))
+
+
+
